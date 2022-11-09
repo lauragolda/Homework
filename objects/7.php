263cd4e8f1ebc279ae89c06c19d458485d509239
Laura Goldštrema <112887957+lauragolda@users.noreply.github.com>
2022-11-09 09:51:03 +0200

    Update 7.php

--- objects/7.php
+++ objects/7.php
@@ -7,39 +7,18 @@ class Dog
     private Dog $mother;
     private Dog $father;
 
-    public function __construct(string $name, string $sex, Dog $mother = null, Dog $father = null)
-    {
+    function __construct(string $name,string $sex) {
         $this->name = $name;
-        $this->sex = $sex;
-        if (isset($mother)) {
-            $this->mother = $mother;
-        }
-        if (isset($father)) {
-            $this->father = $father;
-        }
+        $this->sex= $sex;
+
     }
 
-    public function getName(): string
-    {
-        return $this->name;
+    public function fathersName(): string {
+        return $this->father->name ?? "Unknown";
     }
 
-    public function fathersName(): string
-    {
-        if (!isset($this->father)) {
-            return "Unknown";
-        }
-        return $this->father->name;
-    }
-
-    public function hasSameMotherAs(Dog $anotherDog): bool
-    {
-        return (
-            isset($this->mother) &&
-            isset($anotherDog->mother) &&
-            $this->mother->name === $anotherDog->mother->name
-        );
+    public function hasSameMotherAs(Dog $otherDog): bool {
+        return $this-> mother === $otherDog-> mother;
     }
 }
-
 //Nesapratu kā tālāk pildīt.
